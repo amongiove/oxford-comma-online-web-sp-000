7abb41906e2f87792792599353a32a8bc9bafa37
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-09 01:28:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,9 @@ def oxford_comma(array)
     return array.join
   elsif array.size == 2
     return array.join(" and ")
+  elsif array.size >= 3
+    last_element = array.pop
+    first_few =array.join(", ")
+    first_few + ", and " + last_element
   end
 end
